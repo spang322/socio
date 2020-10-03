68174f8721d6c3f3fa9acdd86dd04efc55fd55e6
Reva Denis <deniska.reva@gmail.com>
2020-10-03 13:55:48 +0300

    SOCIO Project

---
+++ SOCIO/Assets/Scripts/Error1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Globalization;
+
+public class Error1 : MonoBehaviour
+{
+
+    public InputField Field;
+    
+    public int nim;
+    
+    void Update()
+    {
+    
+        nim = Convert.ToInt32(Field.text);
+    }
+    
+}
