362afd2fd2b68915be757c923cbb73caec9cffd9
Nikita Kurylev <n.kurilev@gmail.com>
2020-10-26 20:12:22 +0300

    Started project from scratch

---
+++ newsocio/Assets/Scripts/LabelGame.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LabelGame : MonoBehaviour
+{
+    //TODO: красивый переход
+    Dictionary<string, string> labels;
+    string currentName, currentLabel;
+    //Transition transition;
+    [SerializeField]
+    InputField nameField, labelField;
+    [SerializeField]
+    GameObject lastPlayerButton, nextPlayerButton;
+    void Start()
+    {
+        labels = new Dictionary<string, string>();
+        lastPlayerButton.SetActive(false);
+        //transition = GetComponent<Transition>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void NextPlayer()
+    {
+        SavePlayer();
+        if (labels.Count == 5) 
+            lastPlayerButton.SetActive(true);
+        if (labels.Count == 11)
+            nextPlayerButton.SetActive(false);
+    }
+    public void LastPlayer()
+    {
+        SavePlayer();
+
+    }
+    void SavePlayer()
+    {
+        labels.Add(nameField.text, labelField.text);
+        nameField.text = "";
+        labelField.text = "";
+    }
+}
---
+++ newsocio/Assets/Scripts/Transition.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Transition : MonoBehaviour
+{
+
+    public void Shift(int transition)
+    {
+        targetShift = new Vector3(transition,0);
+        isShifting = true;
+    }
+    Vector3 targetShift;
+    bool isShifting;
+    RectTransform rectTransform;
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+    void Update()
+    {
+        if (isShifting)
+        {
+            rectTransform.anchoredPosition = Vector3.Lerp(rectTransform.anchoredPosition, targetShift, 0.3f);
+            if (Vector3.Distance(transform.position, targetShift) < 1)
+            {
+                rectTransform.anchoredPosition = targetShift;
+                isShifting = false;
+            }
+        }
+    }
+}
