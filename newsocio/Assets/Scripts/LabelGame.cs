9764c50e2f179917583ce9cd4b108523f8972157
Nikita Kurylev <n.kurilev@gmail.com>
2020-10-26 20:29:37 +0300

    -

--- newsocio/Assets/Scripts/LabelGame.cs
+++ newsocio/Assets/Scripts/LabelGame.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+[RequireComponent(typeof(SceneTransition))]
 public class LabelGame : MonoBehaviour
 {
     //TODO: красивый переход
@@ -13,10 +13,13 @@ public class LabelGame : MonoBehaviour
     InputField nameField, labelField;
     [SerializeField]
     GameObject lastPlayerButton, nextPlayerButton;
+    SceneTransition sceneTransition;
     void Start()
     {
         labels = new Dictionary<string, string>();
         lastPlayerButton.SetActive(false);
+        DontDestroyOnLoad(gameObject);
+        sceneTransition = GetComponent<SceneTransition>();
         //transition = GetComponent<Transition>();
     }
 
@@ -36,7 +39,7 @@ public class LabelGame : MonoBehaviour
     public void LastPlayer()
     {
         SavePlayer();
-
+        sceneTransition.loadScene(2);
     }
     void SavePlayer()
     {
